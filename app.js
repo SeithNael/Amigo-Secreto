eeb309c3f4e2bb9beed6e57a3635083c034a7130
Paul <paulllorente@gmail.com>
2025-02-13 00:00:35 -0500

    Trabajo terminado

--- app.js
+++ app.js
@@ -7,3 +7,66 @@ Validar entrada: Si el campo de texto está vacío, el programa mostrará una al
 Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 
 Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
+//let listaNombresSorteados = [];
+
+
+let ListaAmigosArray = [];
+const MuestraHTML = document.getElementById("listaAmigos");
+const resultadoHTML = document.getElementById("resultado");
+Iniciar();
+
+//Funcion para generar indicaciones
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+       return;
+      console.log(texto) 
+}
+
+
+
+function Iniciar() {
+ //Asigna Texto a N1 
+    asignarTextoElemento("h1"," Juego Amigo Secreto");
+}
+
+function limpiartexto() {
+    document.querySelector('#amigos').value = '';
+}
+
+
+function agregarAmigo(){
+    const box = document.getElementById("amigos").value;
+    if (box==""){
+        alert("Ingresa un nombre válido");
+    }else {
+        ListaAmigosArray.push(box)
+        
+        MuestraHTML.innerHTML += `<li> ${box}</li>`
+        limpiartexto();
+    }         
+    
+}
+
+ function sortearAmigo(){
+        
+        // Verificacion Lista  console.log(ListaAmigosArray);
+        let cantidadAmigos=ListaAmigosArray.length;
+        /*   Verificacion cantidad
+        console.log(cantidadAmigos);*/
+        let sorteado= Math.floor(Math.random()*cantidadAmigos);
+        /* Verificacion posicion sorteada
+        console.log(sorteado);
+        Verificacion Persona sorteada
+        console.log(ListaAmigosArray[sorteado]);*/
+        resultadoHTML.innerHTML += `<li> El amigo secreto es ${ListaAmigosArray[sorteado]}</li>`;
+        asignarTextoElemento("h1","Juego Terminado");
+              
+          
+          setTimeout(() => {  
+          location.reload()}, 5000);  
+          setTimeout(() => {  
+            alert("El juego se reiniciara")}, 3000);  
+  
+        
+    }
